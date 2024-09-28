4197850895315ffecf530aa7d11187976e3d7ec2
Sarthiki Katiyar <143502919+Sarthiki-Katiyar@users.noreply.github.com>
2024-09-28 20:24:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+//objects 
+{const student=
+{
+  fullname:"sarthiki",
+  marks:85.5,
+  printmarks:function(){
+    console.log("marks= ",this.marks)//this keyword ko use kia h qki marks ko directly access nhi kia ja skata h
+
+  },
+};}
+// prototype in js: js obj have some state and behaviour(propeties or methods) and objects ave some special property called prototype (object k andr ek object)
+//we can set prototype using  __proto__
+{let arr=["apple","mango","banana"];}
+//arr object me prototpe pahle se hi define honge like push pop and all these kind of functions
+
+ {
+    const emp={
+          
+    }
+ }
+ 
+ 
+ 
